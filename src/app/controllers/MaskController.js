02d8e40e48e5f8c925557cc19305369ddbd35b92
Iago <iagonobre22@gmail.com>
2020-07-03 22:25:51 -0300

    Total add

--- src/app/controllers/MaskController.js
+++ src/app/controllers/MaskController.js
@@ -3,7 +3,18 @@ import Mask from '../schemas/Mask';
 class MaskController {
   async index(req, res) {
     const masks = await Mask.find();
-    return res.json(masks);
+
+    const totalMasks = await Mask.count();
+    const totalActive = await Mask.count({ status: 1 });
+    const totalUse = await Mask.count({ use: 1 });
+
+    const result = {
+      totalActive,
+      totalUse,
+      totalMasks,
+      masks
+    };
+    return res.json(result);
   }
 }
 
